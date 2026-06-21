ef9ce7ea035b3cb3553ce2245eea2e2344bbd935
Jacques Thurling <jthurling78@gmail.com>
2026-06-21 11:49:12 +0200

    Move to C++

---
+++ forge.lua
@@ -0,0 +1,18 @@
+return {
+  project = {
+    name = "ml",
+    type = "executable",
+    standard = "20",
+    
+  },
+  testing = false,
+  dependencies = {
+   direct = {},
+   conan = {}
+  },
+  resources = {
+    files = {}
+  },
+  scripts = {},
+  features = {}
+}
